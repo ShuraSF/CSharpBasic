cdcdf8f29435e919da6a8153ef93ad69868074d6
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-04-26 09:50:10 +0300

    Простейший калькулятор с вызовом методов

---
+++ FunctionsAndMethods_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Функции и методы C#
+    // Простейший калькулятор через методы и функции
+
+
+    class Program
+    {
+        static int Sum(int value_1, int value_2)
+        {
+            return value_1 + value_2;
+        }
+        static void PrintResult(int result)
+        {
+            WriteLine($"Результат сложения : {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(ReadLine());
+            b = int.Parse(ReadLine());
+
+            c = Sum(a, b);
+
+            PrintResult(c);
+
+            WriteLine("============================");
+
+
+        }
+    }
+}
