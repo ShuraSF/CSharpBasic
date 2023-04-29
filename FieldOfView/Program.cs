a3301be2e936cd748b3ccdf163a52f4ce55e69fe
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-04-29 11:32:35 +0300

    Добавил конфликты областей видимостей

---
+++ FieldOfView/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Область видимости, или контекст переменной
+    // Конфликты областей видимостей
+
+    
+    class MyClass
+    {
+
+    }
+    class Program
+    {
+        static int b = 11;
+        static void Foo()
+        {
+            int b = 9;
+
+            WriteLine(b);
+            ReadLine();
+        }
+
+       
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+        
+    }
+}
