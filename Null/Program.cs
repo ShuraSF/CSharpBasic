0e63422aa9eef5910710230bfc4cee48d11cff99
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-01 11:20:37 +0300

    Добавил null

---
+++ Null/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Что такое null
+
+    // Если объект является классом, т.е. ссылочным типом - то по умолчанию значение null
+    // а если значимое значение, т.е. структура - то значение по умолчанию юудет исходя из типа данных
+    // null - по сути пустая ссылка которая находится в стеке
+    class MyClass
+    {
+    }
+    struct MyStruct
+    {
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool b;
+            
+            int a;
+
+            double d;
+
+            Random random;
+
+            int[] arr;
+
+            MyClass myClass;
+
+            MyStruct myStruct;
+
+        }
+
+
+    }
+}
