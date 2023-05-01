23b91222fd0c55a4defd611ac1b3b7d772ff9518
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-01 11:28:30 +0300

    Добавил null объеденение с простой проверкой через if/else

---
+++ NullIntegration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Оператор null объединение
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            string result;
+            if (true == null)
+            {
+                result = "Нет данных";
+            }
+            else
+            {
+                result = str;
+            }
+            WriteLine(result);
+        }
+
+
+    }
+}
