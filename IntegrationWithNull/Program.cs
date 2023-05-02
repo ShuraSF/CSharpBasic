51f93863417f984d2384de66e98fd2e660f89694
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-02 10:48:17 +0300

    Проверка на null через операторы объеденения ?? и операторы присваивания ??=

---
+++ IntegrationWithNull/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Оператор присванивания объеденения со значением NULL ??=
+    // опретор объеденения ??
+    // опертор присваивания ??=
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            str ??= string.Empty;
+
+            string result = str ??= "default string";
+
+            WriteLine($"Количество символов в строке {result.Length}");
+        }
+    }
+}
