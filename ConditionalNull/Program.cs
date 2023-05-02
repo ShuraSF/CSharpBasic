4c321491c8f39a8fb38d5e0edeccf0734085f2eb
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-02 11:48:17 +0300

    Null?

---
+++ ConditionalNull/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+
+    // Оператор условного null
+
+    class Program
+    {
+        static int[] GetArray()
+        {
+            int[] myArray = null;
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray();
+
+            // для проверки на null мы просто добавили ?
+            // поэтому в консоле не будет ошибки
+            WriteLine($"Сумма элементов массива {myArray?.Sum()}");
+        }
+    }
+}
