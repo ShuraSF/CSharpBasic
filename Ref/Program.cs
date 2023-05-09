bd3d78e8ac4463a593c10685695d24e1b2955965
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-09 12:39:58 +0300

    Добавлено метод ref

---
+++ Ref/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    public class Program
+    {
+        static void Foo(ref int a)
+        {
+            a = -5;
+        }
+        static void Main(string[] args)
+        {
+            int a = 2;
+
+            Foo(ref a);
+
+            WriteLine(a); // с методов ref выводится -5
+        }
+    }
+}
