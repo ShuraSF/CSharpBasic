ec97fdf903b1859a8233b24116b68c45e76725b9
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-05-18 10:16:54 +0300

    Ref array

--- Ref/Program.cs
+++ Ref/Program.cs
@@ -9,12 +9,34 @@ namespace CrossProject
 {
     public class Program
     {
-        static void Foo(ref int a)
+        static void Foo(ref int a) //при помощи ref можно возвращать немколько методов, но этим нельзя злоупотреблять
         {
             a = -5;
         }
+        static void Doo(MyClass myClass)
+        {
+            myClass.a = -5;
+        }
+        static void Bar(int[] arr)
+        {
+            arr = null; //null разрывает связь ссылки с управляемой кучей
+        }
+        struct MyStruct
+        {
+            public int a;
+            public double b;
+            public float c;
+        }
         static void Main(string[] args)
         {
+            int[] myArray = { 1, 4, 6 };
+
+            Bar(myArray);
+            
+            MyClass myClass = new MyClass();
+
+            Doo(myClass);
+            
             int a = 2;
 
             Foo(ref a);
