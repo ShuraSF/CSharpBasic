7fa3e62b8d33a727616c3df50c9b081497f82f2a
Саша Евдокимов <thefreeskystyle@gmail.com>
2023-07-16 10:33:44 +0300

    Добавлен проект Testing

---
+++ Testing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CrossProject
+{
+    // Ссылочные локальные переменные
+
+    // Возвращаемые ссылочные переменные
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 7;
+            double res = a / b - ((a / b) % 0.01); // истинный результат минус остаток от деления
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
